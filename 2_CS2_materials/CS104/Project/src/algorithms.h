6862b93aa623296e957d97d41e41620b8f12ba09
deynklarys <144914287+deynklarys@users.noreply.github.com>
2024-11-14 20:50:49 +0800

    refactor(project/algorithms.h): add function declarations

--- 2_CS2_materials/CS104/Project/src/algorithms.h
+++ 2_CS2_materials/CS104/Project/src/algorithms.h
@@ -1,6 +1,32 @@
 #define ALGORITHMS_H
 #ifndef ALGORITHMS_H
 
+void printArray(int array[], int size);
+void swap(int *a, int *b);
+
+void bubbleSort(int array[], int size);
+void selectionSort(int array[], int size);
+void insertionSort(int array[], int size);
+
+bool isArraySorted(int array[], int size);
+void shuffle(int array[], int size);
+void randomSort(int array[], int size);
+
+void merge(int array[], int leftIndex, int mid, int rightIndex);
+void mergeSort(int array[], int leftIndex, int rightIndex);
+
+int partitionLomuto(int array[], int first, int last);
+int partitionHoare(int array[], int first, int last);
+void quickSortLomuto(int array[], int first, int last);
+void quickSortHoare(int array[], int first, int last);
+
+void countRadixSort(int array[], int size, int placeValue);
+void radixSort(int array[], int size);
+
+void heapify(int array[], int size, int i);
+void heapSort(int array[], int size);
+
+void sort(int array[], int size, int sortType);
 
 
 
