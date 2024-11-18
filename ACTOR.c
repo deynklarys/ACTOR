213d91fdd6fbc79c375427e57d60593f44a485ac
deynklarys <144914287+deynklarys@users.noreply.github.com>
2024-11-18 22:38:39 +0800

    feat: add functions from algorithms.c to ACTOR.c for testing, define true and false instead of using bool.h

--- ACTOR.c
+++ ACTOR.c
@@ -6,6 +6,8 @@
 #define ARRAY_MAX_LENGTH 1000
 #define SUBSTRINGS_MAX_SUBSTRINGS 100 
 #define SUBSTRINGS_MAX_LENGTH 500
+#define true 1
+#define false 0
 
 
 int setWidth = 80;
@@ -44,6 +46,34 @@ void algorithms();
     void partitionScheme(); // for quick sort
     void digitOrder(); // for radix sort
 void about();
+
+// Arrays Function Declarations
+void printArray(int array[], int size);
+void swap(int *a, int *b);
+
+void bubbleSort(int array[], int size);
+void selectionSort(int array[], int size);
+void insertionSort(int array[], int size);
+
+int isArraySorted(int array[], int size);
+void shuffle(int array[], int size);
+void randomSort(int array[], int size);
+
+void merge(int array[], int leftIndex, int mid, int rightIndex);
+void mergeSort(int array[], int leftIndex, int rightIndex);
+
+int partitionLomuto(int array[], int first, int last);
+int partitionHoare(int array[], int first, int last);
+void quickSortLomuto(int array[], int first, int last);
+void quickSortHoare(int array[], int first, int last);
+
+void countRadixSort(int array[], int size, int placeValue);
+void radixSort(int array[], int size);
+
+void heapify(int array[], int size, int i);
+void heapSort(int array[], int size);
+
+void sort(int array[], int size, int sortType);
 
 
 int main () {
@@ -302,6 +332,44 @@ void welcomeScreen () {
   hideCursor();
 }
 
+void sorting() {
+  system("cls");
+  programHeader("Sorting Algorithms");
+  int arrSize;
+  int givenArray[ARRAY_MAX_LENGTH];
+  printf("Enter array size: ");
+  scanf("%d", &arrSize);
+  printf("Enter your elements in the array:\n");
+  for (int i = 0; i < arrSize; i++) {
+    printf("\tarray[%d]: ", i);
+    scanf("%d", &givenArray[i]);
+  }
+
+  system("cls");
+
+  printf("Given array: ");
+  for (int i = 0; i < arrSize; i++) {
+    printf("%d ", givenArray[i]);
+  }
+  printf("\n");
+
+  char *sortMenu[] = {"Selection Sort", "Bubble Sort", "Insertion Sort", "Count Sort", "Random Sort", "Merge Sort", "Quick Sort", "Radix Sort", "Heap Sort"};
+  int sortMenuSize = sizeof(sortMenu)/sizeof(sortMenu[0]); 
+
+  printf("What type of sorting algorithm do you want to choose?\n");
+  printMenu(sortMenu, sortMenuSize);
+  int sortType;
+  scanf("%d", &sortType);
+
+  system("cls");
+  sort (givenArray, arrSize, sortType);
+
+  printf("\n\n");
+
+  displayCenterText("Press Any Key To Exit");
+  hideCursor();
+  anyChar = _getch();
+}
 void algorithms() {
   char *algorithmsMenu[] = {"Searching", "Sorting", "Exit", "Quit"};
   int algoMenuSize = sizeof(algorithmsMenu) / sizeof(algorithmsMenu[0]);
@@ -337,11 +405,12 @@ void algorithms() {
           anyChar = _getch();
           break;
         case 2:
-          system("cls");
+          /*system("cls");
           programHeader("Sorting Algorithms");
           printf("\n\nFunction is not yet done...\n\n");
           displayCenterText("Press Any Key To Exit");
-          anyChar = _getch();
+          anyChar = _getch();*/
+          sorting();
           break;
         case 3:
           system("cls");
@@ -402,6 +471,310 @@ void about() {
 
 
 
+// Array Functions
+// Utility functions
+void printArray(int array[], int size) {
+  int i;
+  for (i = 0; i < size; i++) {
+    printf("%d ", array[i]);
+  }
+  printf("\n\n");
+}
+void swap(int *a, int *b) {
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
+// Sort functions
+void selectionSort(int array[],  int size) {
+  int i, j, min, temp;
+  for (i = 0; i < size - 1; i++) {
+    min = i;
+    for (j = i + 1; j < size; j++) {
+      if (array[j] < array[min]) {
+        min = j;
+      }
+    }
+    if (min != i) {
+      swap(array[i], array[min]);
+    }
+  }
+}
+void bubbleSort (int array[], int size) {
+  int i, j, temp;
+  for (i = 0; i < size - 1; i++) {
+    for (j = 0; j < size - i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        swap(array[j], array[j + 1]);
+      }
+    }
+  }
+}
+void insertionSort(int array[], int size) {
+  int i,j, temp;
+  for (i = 1; i < size; i++) {
+    temp = array[i];
+    j = i - 1;
+    for (j=i-1; j >= 0 && array[j] > temp; j--) {
+      array[j + 1] = array[j];
+    }
+    array[j + 1] = temp;
+  }
+}
+void countSort(int array[], int size) {
+  int max = 0;
+
+  for (int i = 0; i < size; i++)
+    if (array[i] > max)
+      max = array[i];
+
+  int countArraySize = max + 1; 
+
+  int *countArray = (int *)malloc((countArraySize) * sizeof(int));
+  for (int i = 0; i < countArraySize; i++)
+    countArray[i] = 0;
+
+  for (int i = 0; i < size; i++)
+    countArray[array[i]]++;
+  
+  for (int i = 1; i < countArraySize; i++)
+    countArray[i] += countArray[i - 1];
+
+  int *outputArray = (int *)malloc((size) * sizeof(int));
+  for (int i = 0; i < size; i++)
+    outputArray[i] = 0;
+
+  for (int i = size - 1; i >= 0; i--) {
+    outputArray[countArray[array[i]] - 1] = array[i];
+    countArray[array[i]]--;
+  }
+  
+  for (int i = 0; i < size; i++)
+    array[i] = outputArray[i];
+}
+
+int isArraySorted (int array[], int size) {
+  for (int i = 1; i < size; i++) {
+    if (array[i] < array [i - 1]){
+      return false;
+    }
+  }
+  return true;
+}
+void shuffle (int array[], int size) {
+  int i, randomNumber, temp;
+  for (i = 0; i < size; i++) {
+    randomNumber = rand() % size;
+    swap(array[i], array[randomNumber]);
+  }
+}
+void randomSort(int array[], int size) {
+  while (!isArraySorted(array, size)) {
+    shuffle(array, size);
+  }
+}
+
+// Merge sort has another algorithm that is probably more efficient, which uses only one temporary array and takes five parameters in the merge function. It as well requires a base case to stop the recursion. Other algorithm will be added in the future.
+void merge(int array[], int leftIndex, int mid, int rightIndex) {
+  int i, j, k;
+  int leftArrSize = mid - leftIndex + 1;
+  int rightArrSize = rightIndex - mid;
+
+  int *leftArray = (int *)malloc((leftArrSize) * sizeof(int));
+  for (i = 0; i < leftArrSize; i++) 
+    leftArray[i] = 0;
+
+  int *rightArray = (int *)malloc((rightArrSize) * sizeof(int));
+  for (i = 0; i < rightArrSize; i++)
+    rightArray[i] = 0;
+
+  for (i = 0; i < leftArrSize; i++)
+    leftArray[i] = array[leftIndex + i];
+  for (j = 0; j < rightArrSize; j++)
+    rightArray[j] = array[mid + 1 + j];
+
+  i = 0;
+  j = 0;
+  k = leftIndex;
+
+  while (i < leftArrSize && j < rightArrSize) {
+    if (leftArray[i] <= rightArray[j])
+      array[k] = leftArray[i++];
+    else
+      array[k] = rightArray[j++];
+    k++;
+  }
+
+  while (i < leftArrSize) 
+    array[k++] = leftArray[i++];
+
+  while (j < rightArrSize)
+    array[k++] = rightArray[j++];
+}
+void mergeSort (int array[], int leftIndex, int rightIndex) {
+  if (leftIndex < rightIndex) {
+    int mid = (leftIndex + rightIndex) / 2;
+    
+    mergeSort(array, leftIndex, mid);
+    mergeSort(array, mid + 1, rightIndex);
+
+    merge(array, leftIndex, mid, rightIndex);
+  }
+}
+
+int partitionLomuto(int array[], int first, int last) {
+    int pivot = array[last];
+    int i = first - 1;
+    for (int j = first; j <= last - 1; j++) {
+      if (array[j] < pivot) {
+        i++;
+        swap(&array[i], &array[j]);
+      }
+    }
+    i++;
+    swap(&array[i], &array[last]);
+    return i;
+}
+int partitionHoare(int array[], int first, int last) {
+    int pivot = array[first];
+    int i = first - 1;
+    int j = last + 1;
+    while (true) {
+      do {
+        i++;
+      } while (array[i] < pivot);
+      do {
+        j--;
+      } while (array[j] > pivot);
+      if (i >= j) {
+        return j;
+      }
+      swap(&array[i], &array[j]);
+    }
+}
+void quickSortLomuto(int array[], int first, int last) {
+  if (first < last) {
+    int partitionIndex = partitionLomuto(array, first, last);
+    quickSortLomuto(array, first, partitionIndex - 1);
+    quickSortLomuto(array, partitionIndex + 1, last);
+  }
+}
+void quickSortHoare(int array[], int first, int last) {
+  if (first < last) {
+    int partitionIndex = partitionHoare(array, first, last);
+    quickSortHoare(array, first, partitionIndex);
+    quickSortHoare(array, partitionIndex + 1, last);
+  }
+}
+
+void countRadixSort (int array[], int size, int placeValue) {
+  int *outputArray = (int *)malloc((size) * sizeof(int));
+  for (int i = 0; i < size; i++)
+    outputArray[i] = 0;
+
+  int countArray[10] = {0};
+
+  for (int i = 0 ; i < size; i++) 
+    countArray[array[i] / placeValue % 10]++;
+
+  for (int i = 1; i < 10; i++)
+    countArray[i] += countArray[i - 1];
+
+  for (int i = size - 1; i >= 0; i--)
+    outputArray[--countArray[array[i] / placeValue % 10]] = array[i];
+
+  for (int i = 0; i < size; i++)
+    array[i] = outputArray[i];
+}
+void radixSort(int array[], int size) {
+  int max = array[0];
+  for (int i = 1; i < size; i++) {
+    if (array[i] > max) {
+      max = array[i];
+    }
+  }
+
+  for (int placeValue = 1; max / placeValue > 0; placeValue *= 10)
+    countRadixSort(array, size, placeValue);
+}
+
+void heapify(int array[], int size, int i) {
+  int largest = i; 
+  int leftIndex = 2 * i + 1; 
+  int rightIndex = 2 * i + 2;
+
+  if (leftIndex < size && array[leftIndex] > array[largest]) 
+    largest = leftIndex;
+
+  if (rightIndex < size && array[rightIndex] > array[largest])
+    largest = rightIndex;
+
+  if (largest != i) {
+    swap(&array[i], &array[largest]);
+
+    heapify(array, size, largest);
+  }
+}
+void heapSort(int array[], int size) {
+  for (int i = size / 2 - 1; i >= 0; i--)
+    heapify(array, size, i);
+
+  for (int i = size - 1; i > 0; i--) {
+    swap(array[0], array[i]);
+
+    heapify(array, i, 0);
+  }
+}
+
+void sort(int array[], int size, int sortType) {
+  printf("Given Array:\n");
+  printArray(array, size);
+
+  switch (sortType) {
+    case 1:
+      printf("Selection Sort:\n");
+      selectionSort(array, size);
+      break;
+    case 2:
+      printf("Bubble Sort:\n");
+      bubbleSort(array, size);
+      break;
+    case 3:
+      printf("Insertion Sort:\n");
+      insertionSort(array, size);
+      break;
+    case 4:
+      printf("Count Sort:\n");
+      countSort(array, size);
+      break;
+    case 5:
+      printf("Random Sort:\n");
+      randomSort(array, size);
+      break;
+    case 6:
+      printf("Merge Sort:\n");
+      mergeSort(array, 0, size - 1);
+      break;
+    case 7:
+      printf("Quick Sort (Lomuto):\n");
+      quickSortLomuto(array, 0, size - 1);
+      break;
+    case 8:
+      printf("Radix Sort:\n");
+      radixSort(array, size);
+      break;
+    case 9:
+      printf("Heap Sort:\n");
+      heapSort(array, size);
+      break;
+    default:
+      printf("Invalid choice\n");
+      break;
+  }
+
+  printArray(array, size);
+}
 
 
 
