04c44701f33ceaae3797e3a72bd49481dba5ddf8
deynklarys <144914287+deynklarys@users.noreply.github.com>
2024-11-20 22:25:52 +0800

    chore: add function declarations

---
+++ src/arrays.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <windows.h>
+#include <conio.h> // for _getch()
+#include "utility.h"
+
+void traverse(int arr[], int size);
+int search(int arr[], int size, int key);
+void insert(int arr[], int *n, int key, int position);
+void delete(int arr[], int *n, int position);
+void mergeArrays(int arr1[], int size1, int arr2[], int size2, int mergedArray[]);
+void merge() {
+  int arr1[] = {1, 2, 3, 4, 5};
+  int arr2[] = {6, 7, 8, 9, 10};
+  int size1 = sizeof(arr1) / sizeof(arr1[0]);
+  int size2 = sizeof(arr2) / sizeof(arr2[0]);
+  int mergedSize = size1 + size2;
+  int mergedArray[mergedSize];
+
+  mergeArrays(arr1, size1, arr2, size2, mergedArray);
+
+  printf("Merged array: ");
+  for (int i = 0; i < mergedSize; i++) {
+      printf("%d ", mergedArray[i]);
+  }
+  printf("\n");
+}
+
+int main () {
+
+  return 0;
+}
+
+void traverse (int arr[], int size) {
+  for (int i = 0; i < size; i++) {
+    printf("%d ", arr[i]);
+  }
+  printf("\n");
+}
+
+int search (int arr[], int size, int key) {
+  for (int i = 0; i < size; i++) {
+    if (arr[i] == key) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+void insert (int arr[], int *n, int key, int position) {
+  for (int i = *n - 1; i >= position; i--) {
+    arr[i + 1] = arr[i];
+  }
+  arr[position] = key;
+  (*n)++;
+}
+
+void delete (int arr[], int *n, int position) {
+  for (int i = position; i < *n - 1; i++) {
+    arr[i] = arr[i + 1];
+  }
+  (*n)--;
+}
+
+void mergeArr(int arr1[], int size1, int arr2[], int size2, int mergedArray[]) {
+    int i, j;
+
+    // Copy elements from the first array
+    for (i = 0; i < size1; i++) {
+        mergedArray[i] = arr1[i];
+    }
+
+    // Copy elements from the second array
+    for (j = 0; j < size2; j++) {
+        mergedArray[i + j] = arr2[j];
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
--- src/strings.c
+++ src/strings.c
@@ -3,7 +3,66 @@
 #include <conio.h> // for _getch()
 #include "utility.h"
 
+// Declarations
+int getChar (char *string[], int n); // returns the nth character of the string
+void putChar (char *string[], int n, char charToBeInserted); // sets the nth character of the string to charToBeInserted
+int strLength (char *string[]); // returns the length of the string
+int position (char *string[], char *stringToFind[]); // returns the position of the first occurrence of stringToFind in string
+char *concatenate (char *string1[], char *string2[]); // returns the concatenation of string1 and string2
+char *substring (char *string[], int position, int length); // returns the substring of string starting at position and of length length
+void deleteStr (char *string[], int position, int length); // deletes the substring of string starting at position and of length length
+char *insertStr (char *string[], char *stringToInsert[], int position); // inserts stringToInsert into string at position
+int compareStr (char *string1[], char *string2[]); // returns 0 if string1 and string2 are equal, -1 if string1 < string2, 1 if string1 > string2
+
+
+
+
 int main () {
 
+  return 0;
+}
+
+//Definitions
+
+int getChar (char *string[], int n) {
+  // Function definition
+  return 0;
+}
+
+void putChar (char *string[], int n, char charToBeInserted) {
+  // Function definition
+}
+
+int strLength (char *string[]) {
+  // Function definition
+  return 0;
+}
+
+int position (char *string[], char *stringToFind[]) {
+  // Function definition
+  return 0;
+}
+
+char *concatenate (char *string1[], char *string2[]) {
+  // Function definition
+  return NULL;
+}
+
+char *substring (char *string[], int position, int length) {
+  // Function definition
+  return NULL;
+}
+
+void deleteStr (char *string[], int position, int length) {
+  // Function definition
+}
+
+char *insertStr (char *string[], char *stringToInsert[], int position) {
+  // Function definition
+  return NULL;
+}
+
+int compareStr (char *string1[], char *string2[]) {
+  // Function definition
   return 0;
 }
