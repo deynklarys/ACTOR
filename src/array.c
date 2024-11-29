f906de895f42b2e16d9523350a597e59f7bcf477
deynklarys <144914287+deynklarys@users.noreply.github.com>
2024-11-30 00:58:19 +0800

    fix: remove unnecessary functions

--- src/array.c
+++ src/array.c
@@ -32,13 +32,7 @@ typedef struct {
 void traverseArray();
 void searchArray();
 void insertArray();
-  void insertAtBeginning();
-  void insertAtEnd();
-  void insertAtMiddle();
 void deleteArray();
-  void deleteFromBeginning();
-  void deleteFromEnd();
-  void deleteFromMiddle();
 void mergeArray();
 void sortArray();
 
@@ -228,112 +222,18 @@ void searchArray() {
 }
 
 void insertArray() {
-  char *insertMenu[] = {"Insert at the beginning", "Insert at the middle", "Insert at the end", "Exit"};
-  int insertMenuSize = sizeof(insertMenu) / sizeof(insertMenu[0]);
-  int chosenOption;
-
-
-  do {
-    programHeader("Arrays");
-    printMenu(insertMenu, insertMenuSize);
-    if (scanf("%d", &chosenOption) != 1) {
-      clearInputBuffer(); // Clear invalid input
-      printf("Invalid input. Please enter a number.\n");
-      continue;
-    }
-
-    switch (chosenOption) {
-      case 1:
-        functionNotDone("Insert at the beginning");
-        insertAtBeginning();
-        break;
-      case 2:
-        functionNotDone("Insert at the middle");
-        insertAtMiddle();
-        break;
-      case 3:
-        functionNotDone("Insert at the end");
-        insertAtEnd();
-        break;
-      default:
-        printf("Invalid choice. Please choose a valid option.\n");
-    }
-
-    // if (chosenOption != 7) {
-    //   printf("Press any key to continue...\n");
-    //   getch(); // Wait for user input before continuing
-    // }
-
-  } while (chosenOption != insertMenuSize);
+  // Implementation of insert
 }
 
-void insertAtBeginning() {
-    // Implementation of insertAtBeginning
-}
-
-void insertAtEnd() {
-    // Implementation of insertAtEnd
-}
-
-void insertAtMiddle() {
-    // Implementation of insertAtMiddle
-}
 
 void deleteArray() {
-  char *deleteMenu[] = {"Delete at the beginning", "Delete at the middle", "Delete at the end", "Exit"};
-  int deleteMenuSize = sizeof(deleteMenu) / sizeof(deleteMenu[0]);
-  int chosenOption;
-
-
-  do {
-    programHeader("Arrays");
-    printMenu(deleteMenu, deleteMenuSize);
-    if (scanf("%d", &chosenOption) != 1) {
-      clearInputBuffer(); // Clear invalid input
-      printf("Invalid input. Please enter a number.\n");
-      continue;
-    }
-
-    switch (chosenOption) {
-      case 1:
-        functionNotDone("Delete at the beginning");
-        deleteFromBeginning();
-        break;
-      case 2:
-        functionNotDone("Delete at the middle");
-        deleteFromMiddle();
-        break;
-      case 3:
-        functionNotDone("Delete at the end");
-        deleteFromEnd();
-        break;
-      default:
-        printf("Invalid choice. Please choose a valid option.\n");
-    }
-
-    // if (chosenOption != 7) {
-    //   printf("Press any key to continue...\n");
-    //   getch(); // Wait for user input before continuing
-    // }
-
-  } while (chosenOption != deleteMenuSize);}
-
-void deleteFromBeginning() {
-    // Implementation of deleteFromBeginning
-}
-
-void deleteFromEnd() {
-    // Implementation of deleteFromEnd
-}
-
-void deleteFromMiddle() {
-    // Implementation of deleteFromMiddle
+  // Implementation of delete
 }
 
 void mergeArray() {
-    // Implementation of merge
+  // Implementation of merge
 }
 
 void sortArray() {
-    // Implementation of sort
+  // Implementation of sort
 }
