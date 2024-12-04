b475fd95ed839ef4784ca60e4e051c502a7a38c3
deynklarys <144914287+deynklarys@users.noreply.github.com>
2024-12-04 08:30:36 +0800

    refactor: update stack handling and improve menu interaction (guided by linkedLists.c)

--- src/stacks.c
+++ src/stacks.c
@@ -37,7 +37,7 @@ typedef struct {
 } Stack;
 
 typedef struct {
-  Stack stackList;
+  Stack list;
   int chosenDataType;
 } StackResult;
 
@@ -63,48 +63,73 @@ void *scanData(char prompt[], DataType dataType); // can be used for all data st
 void freeAll(Stack *stack);
 
 int main () {
-  char *stacksMenu[] = {"Add an item", "Remove an item","Look for an item", "Check if the stack is full", "Check if the stack is empty", "Exit"};
-  int stacksMenuSize = sizeof(stacksMenu) / sizeof(stacksMenu[0]);
-  int chosenOption;
 
+  programHeader("Stacks");
 
-  do {
-    programHeader("Linked List");
-    printMenu(stacksMenu, stacksMenuSize);
-    if (scanf("%d", &chosenOption) != 1) {
-      clearInputBuffer(); // Clear invalid input
-      printf("Invalid input. Please enter a number.\n");
-      continue;
+  Stack stack;
+
+  while (1) {
+    StackResult stackResult = initializeStacks();
+    stack = stackResult.list;
+    stack.stackDataType = stackResult.chosenDataType;
+    if (stack.stackDataType == -1) {
+      system("cls");
+      freeAll(&stack);
+      return 0;
     }
 
-    switch (chosenOption) {
-      case 1:
-        functionNotDone("Add an item");
-        push();
-        break;
-      case 2:
-        functionNotDone("Remove an item");
-        pop();
-        break;
-      case 3:
-        functionNotDone("Look for an item");
-        peek();
-        break;
-      case 4:
-        functionNotDone("Check if the stack is full");
-        isFull();
-        break;
-      case 5:
-        functionNotDone("Check if the stack is empty");
-        isEmpty();
-        break;
-      case 6:
-        promptExit();
-        break;
-      default:
-        printf("Invalid choice. Please choose a valid option.\n");
-    }
-  } while (chosenOption != stacksMenuSize);
+    char *stacksMenu[] = {"Add an item", "Remove an item","Look for an item", "Check if the stack is full", "Check if the stack is empty", "Exit"};
+    int stacksMenuSize = sizeof(stacksMenu) / sizeof(stacksMenu[0]);
+    int chosenOption;
 
+
+    do {
+      programHeader("Stacks Operations");
+      printMenu(stacksMenu, stacksMenuSize);
+      getCursorPos(&cursorXpos, &cursorYpos);
+      if (scanf("%d", &chosenOption) != 1) {
+        clearLines(cursorYpos + 1, cursorYpos + 1);
+        moveCursor(0, cursorYpos + 1);
+        clearInputBuffer();
+        printf("Invalid input. Please enter a number.\n");
+        clearWord(cursorYpos, cursorXpos, SET_WIDTH);
+        continue;
+      }
+
+      clearLines(cursorYpos + 1, cursorYpos + 1);
+      moveCursor(0, cursorYpos + 2);
+
+      switch (chosenOption) {
+        case 1:
+          functionNotDone("Add an item");
+          push();
+          break;
+        case 2:
+          functionNotDone("Remove an item");
+          pop();
+          break;
+        case 3:
+          functionNotDone("Look for an item");
+          peek();
+          break;
+        case 4:
+          functionNotDone("Check if the stack is full");
+          isFull();
+          break;
+        case 5:
+          functionNotDone("Check if the stack is empty");
+          isEmpty();
+          break;
+        case 6:
+          promptExit();
+          break;
+        default:
+          moveCursor(0, cursorYpos + 1);
+          printf("Invalid choice. Please choose a valid option.\n");
+      }
+      clearWord(cursorYpos, cursorXpos, SET_WIDTH);
+    } while (chosenOption != stacksMenuSize);
+    system("cls");
+  }
   return 0;
 }
